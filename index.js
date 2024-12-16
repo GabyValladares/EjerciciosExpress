d025865a898cfcff8bc247d2e790cf198b7aeeae
GabyValladares <126087266+GabyValladares@users.noreply.github.com>
2024-12-16 10:49:15 -0500

    all

--- index.js
+++ index.js
@@ -16,7 +16,27 @@ app.get('/nombre',(req,res)=>{
     res.json(lista);
 });
 
+app.get('/milton',(req, res)=>{
+    res.send('Hola mi nombre es Milton Ramos tengo 22 años, soy de la ciudad de Quito');
+});
+
+app.get('/suma',(req, resp)=>{
+    let n1=2;
+    let n2=2;
+    let suma = n1+n2;
+    resp.send(suma+'');
+});
+
+app.get('/suma/:n1',(req, resp)=>{
+    let num1 = parseInt(req.params.n1);
+    let num2 = 9;
+    let suma = num1 + num2;
+    resp.send('el resultado de la suma es: '+suma);
+});
+
 app.listen(3001,()=>{
     console.log ('Correcto');
 });
+
+
 
